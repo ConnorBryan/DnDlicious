3eef4386e83f61eb86f837e4aa3fefeef2fedd9d
Connor Bryan <cchromium@gmail.com>
2019-04-26 18:37:15 -0500

    ...

--- server/index.js
+++ server/index.js
@@ -1,47 +1,47 @@
 const fs = require("fs");
-const http = require("http");
+// const http = require("http");
 const https = require("https");
 const WebSocket = require("ws");
 
-if (process.env.NODE_ENV === "production") {
-  const cert = fs.readFileSync("server/server.cert", "utf8");
-  const key = fs.readFileSync("server/server.key", "utf8");
-  const server = https.createServer({
-    cert,
-    key
-  });
-  const wsServer = new WebSocket.Server({ server });
-  const connectedSockets = [];
+// if (process.env.NODE_ENV === "production") {
+const cert = fs.readFileSync("server/server.cert", "utf8");
+const key = fs.readFileSync("server/server.key", "utf8");
+const server = https.createServer({
+  cert,
+  key
+});
+const wsServer = new WebSocket.Server({ server });
+const connectedSockets = [];
 
-  wsServer.on("connection", socket => {
-    connectedSockets.push(socket);
+wsServer.on("connection", socket => {
+  connectedSockets.push(socket);
 
-    socket.on("message", message => {
-      connectedSockets.forEach(socket => socket.send(message));
-    });
-
-    socket.on("close", () => {
-      connectedSockets.splice(connectedSockets.indexOf(socket), 1);
-    });
+  socket.on("message", message => {
+    connectedSockets.forEach(socket => socket.send(message));
   });
 
-  server.listen(9000);
-} else {
-  const server = http.createServer();
-  const wsServer = new WebSocket.Server({ server });
-  const connectedSockets = [];
-
-  wsServer.on("connection", socket => {
-    connectedSockets.push(socket);
-
-    socket.on("message", message => {
-      connectedSockets.forEach(socket => socket.send(message));
-    });
-
-    socket.on("close", () => {
-      connectedSockets.splice(connectedSockets.indexOf(socket), 1);
-    });
+  socket.on("close", () => {
+    connectedSockets.splice(connectedSockets.indexOf(socket), 1);
   });
+});
 
-  server.listen(9000);
-}
+server.listen(9000);
+// } else {
+//   const server = http.createServer();
+//   const wsServer = new WebSocket.Server({ server });
+//   const connectedSockets = [];
+
+//   wsServer.on("connection", socket => {
+//     connectedSockets.push(socket);
+
+//     socket.on("message", message => {
+//       connectedSockets.forEach(socket => socket.send(message));
+//     });
+
+//     socket.on("close", () => {
+//       connectedSockets.splice(connectedSockets.indexOf(socket), 1);
+//     });
+//   });
+
+//   server.listen(9000);
+// }
